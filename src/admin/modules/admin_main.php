906ab93f961461a878a3b1117a0787cbd6681764
Alexander Pankiv <alexanderpankiv@gmail.com>
2011-09-15 14:26:02 +0300

    admin design changes

--- src/admin/modules/admin_main.php
+++ src/admin/modules/admin_main.php
@@ -1,7 +1,7 @@
 <?
 	 $q_conf='';
-	?><div style="margin: 10px;">Ласкаво просимо, <b><a href="admin.php?mod=user_profile"><?=$user['first_name']?></a></b>. Ви ввійшли в систему як <b><?=$user['group_name']?></b>.</div><?
-	#$res=sql_do("SELECT COUNT(id) AS cnt FROM requests WHERE processed=0");
+	?><div style="margin: 10px;">Hello, <b><a href="admin.php?mod=user_profile"><?=$user['first_name']?></a></b>. You are logged in as <b><?=$user['group_name']?></b>.</div><?
+	$res=sql_do("SELECT COUNT(id) AS cnt FROM requests WHERE processed=0");
 	#if($q=mysql_fetch_array($res)){
 	#	if($q['cnt']>0){
 	#		echo 'Кількість неопрацьованих запитів: <a href="admin.php?mod=admin_requests&show=2">'.$q['cnt'].'</a>';
